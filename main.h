748370a487c8adc9e9653ad2e04e00435cc588a6
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-05-23 01:38:33 -0700

    simple shell

--- main.h
+++ main.h
@@ -1,9 +1,14 @@
 #ifndef _HEADER_
 #define _HEADER_
+#define BUFSIZE 1024
 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include <unistd.h>
+#include <sys/types.h>
 void strtoken(char *, int);
+void resetFunc(char *str);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
