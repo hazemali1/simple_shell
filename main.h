dfeb477ab902bd4cdabf13b0d237b9b9c8acb155
EsliNsengiyumva <eslinsengiyumva@gmail.com>
2023-05-23 07:35:24 -0700

    simple shell

--- main.h
+++ main.h
@@ -1,14 +1,115 @@
-#ifndef _HEADER_
-#define _HEADER_
-#define BUFSIZE 1024
+#ifndef _HEADER_H_
+#define _HEADER_H_
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+/**###### environ var ######*/
+
+extern char **environ;
+
+/**##### MACROS ######*/
+
+#define BUFSIZE 1024
+#define DELIM " \t\r\n\a"
+#define PRINTER(c) (write(STDOUT_FILENO, c, _strlen(c)))
+
+/**###### LIBS USED ######*/
+
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
-void strtoken(char *, int);
-void resetFunc(char *str);
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/limits.h>
+
+
+
+
+
+/**###### STRING FUNCTION ######*/
+
+char *_strtok(char *str, const char *tok);
+unsigned int check_delim(char c, const char *str);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _putchar(char c);
+int _atoi(char *s);
+void _puts(char *str);
+int _strcmp(char *s1, char *s2);
+int _isalpha(int c);
+void array_rev(char *arr, int len);
+int intlen(int num);
+char *_itoa(unsigned int n);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_strchr(char *s, char c);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
+int print_echo(char **pc);
+
+/**###### MEMORIE  MANGMENT ####*/
+
+void free_env(char **);
+void *fill_an_array(void *, int, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+void *_calloc(unsigned int size);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void free_all(char **input, char *line);
+
+/**###### INPUT Function ######*/
+
+void prompt(void);
+void signal_to_handel(int sig);
+char *_getline(void);
+
+/** ###### Command parser and extractor ###*/
+
+int path_cmd(char **line);
+char *_getenv(char *name);
+char **parse_cmd(char *cmd);
+int handle_builtin(char **cmd, int er);
+void read_file(char *filename, char **argv);
+char *build(char *token, char *value);
+int check_builtin(char **cmd);
+void creat_envi(char **envi);
+int check_cmd(char **tokens, char *line, int count, char **argv);
+void treat_file(char *line, int counter, FILE *fd, char **argv);
+void exit_bul_for_file(char **cmd, char *line, FILE *fd);
+
+/** #### SETENV FUNC #####*/
+
+
+void exit_Shell(char **pd, char *input, char **argv, int c);
+int change_Dir(char **pc, __attribute__((unused))int st);
+int dis_env(__attribute__((unused)) char **pc, __attribute__((unused)) int st);
+int dis_Build(char **pc, __attribute__((unused))int st);
+int exec_Disp(char **pc, int st);
+
+/** #### GETLINE FUNC #####*/
+
+char *_getLineFunc();
+void resetFunc(char *str);
+
+/** ####error handle and Printer ####*/
+void print_number(unsigned int n);
+void print_number_in(int n);
+void print_error(char *line, int c, char **argv);
+void _prerror(char **argv, int c, char **cmd);
+
+
+/**
+ * struct bulltin - contain bultin to handle and function to excute
+ * @command:pointer to char
+ * @fun:fun to excute when bultin true
+ */
+
+typedef struct  bulltin
+{
+	char *command;
+	int (*fun)(char **, int);
+} bul_t;
 
 #endif
