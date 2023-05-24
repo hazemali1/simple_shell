66a760aba9102c44dc72558d0574322b418f535b
hazemali1 <hazemali1343@gmail.com>
2023-05-24 03:53:52 -0700

    path

--- shell.h
+++ shell.h
@@ -63,6 +63,7 @@ int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 char *_strcpy(char *dest, char *src);
 int _putchar(char c);
+void set_arr(char *symo, int *coun, int *Che, char *buf);
 
 
 #endif
